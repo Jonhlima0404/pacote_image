d56a5aa016943d164ae36825c879fa899d2ccc52
Jonhlima0404 <86275811+Jonhlima0404@users.noreply.github.com>
2023-07-11 01:51:54 -0300

    Update pacote.py

--- pacote.py
+++ pacote.py
@@ -1,7 +1,22 @@
-[build-system]
-requires = ["hatchling"]
-build-backend = "hatchling.build"
+import seu_pacote_de_processamento_de_imagens
 
-[project]
-name = "PACKAGENAME"
-version = "PACKAGEVERSION"
+# Obter os módulos presentes no pacote
+modulos = dir(seu_pacote_de_processamento_de_imagens)
+print("Módulos do pacote:")
+print(modulos)
+
+# Iterar sobre os módulos e obter informações sobre as classes e funções
+for modulo in modulos:
+    # Obter as classes presentes no módulo
+    if hasattr(seu_pacote_de_processamento_de_imagens, modulo) and \
+            isinstance(getattr(seu_pacote_de_processamento_de_imagens, modulo), type):
+        classes = dir(getattr(seu_pacote_de_processamento_de_imagens, modulo))
+        print(f"\nClasses do módulo {modulo}:")
+        print(classes)
+
+        # Obter as funções presentes no módulo
+        for classe in classes:
+            if callable(getattr(getattr(seu_pacote_de_processamento_de_imagens, modulo), classe)):
+                funcoes = dir(getattr(getattr(seu_pacote_de_processamento_de_imagens, modulo), classe))
+                print(f"\nFunções da classe {classe}:")
+                print(funcoes)
